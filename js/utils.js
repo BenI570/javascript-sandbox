7e6740f5c266b84797edbc880856d20d0cb01e3e
Benjamin Interlichia <beninterlichiagithub@gmail.com>
2024-07-16 16:46:50 -0400

    changed import statement in utils.js to be able to everything from variables.js

--- js/utils.js
+++ js/utils.js
@@ -1,27 +1,31 @@
 import { navigation_items } from './seedData.js';
-import { getBody } from './variables.js';
+import * as vars from './variables.js';
+
+function createElm(element) {
+	return document.createElement(element);
+}
 
 export function generateNavbar(items) {
 	const navbar = document.querySelector('.d-navbar');
 
-	const logo = document.createElement('span');
+	const logo = createElm('span');
 	logo.innerText = 'Logo';
 	navbar.appendChild(logo);
 
-	const nav = document.createElement('nav');
+	const nav = createElm('nav');
 	navbar.appendChild(nav);
 
-	const ul = document.createElement('ul');
+	const ul = createElm('ul');
 	ul.classList.add('flex');
 	nav.appendChild(ul);
 
 	items.forEach((item) => {
-		const a = document.createElement('a');
+		const a = createElm('a');
 		a.href = item.link;
 		a.innerHTML = '<i class="' + item.icon + '"></i>';
 		a.appendChild(document.createTextNode(item.name));
 		nav.appendChild(a);
-		const li = document.createElement('li');
+		const li = createElm('li');
 		a.appendChild(li);
 	});
 
@@ -31,24 +35,24 @@ export function generateNavbar(items) {
 export function toggleDarkMode() {
 	const lightModeBtn = document.getElementById('lightMode');
 	const darkModeBtn = document.getElementById('darkMode');
-
+	const body = vars.getBody;
 	const isDarkMode = localStorage.getItem('darkMode') === false;
 
 	if (isDarkMode) {
-		getBody.classList.add('darkMode');
+		body.classList.add('darkMode');
 		darkModeBtn.classList.add('hidden');
 		lightModeBtn.classList.remove('hidden');
 	}
 
 	lightModeBtn.addEventListener('click', () => {
-		getBody.classList.remove('darkMode');
+		body.classList.remove('darkMode');
 		darkModeBtn.classList.remove('hidden');
 		lightModeBtn.classList.add('hidden');
 		localStorage.setItem('darkMode', 'disabled');
 	});
 
 	darkModeBtn.addEventListener('click', () => {
-		getBody.classList.add('darkMode');
+		body.classList.add('darkMode');
 		darkModeBtn.classList.add('hidden');
 		lightModeBtn.classList.remove('hidden');
 		localStorage.setItem('darkMode', 'enabled');
